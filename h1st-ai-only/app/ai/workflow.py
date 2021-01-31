c736a54366abdd397692d7517ce0786ea04406a6
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-01-31 15:00:10 -0800

    update h1st-ai-only app.ai.workflow

--- h1st-ai-only/app/ai/workflow.py
+++ h1st-ai-only/app/ai/workflow.py
@@ -12,10 +12,12 @@ class MyWorkflow(H1stWorkflow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.start() \
-            .add(self.first_step)
+        # create Workflow structure if all attributes have been initialized 
+        if hasattr(self, 'first_step'):
+            self.start() \
+                .add(self.first_step)
 
-        self.end()
+            self.end()
 
     def __str__(self):
         return f'{type(self).__name__} #{self.uuid}: ' \
