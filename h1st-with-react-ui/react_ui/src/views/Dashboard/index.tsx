6274e911a5585dca2eff7689c1f18a9b0df08e98
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-05-26 15:36:52 -0700

    add h1st-with-react-ui/react_ui

---
+++ h1st-with-react-ui/react_ui/src/views/Dashboard/index.tsx
@@ -0,0 +1,55 @@
+import { Button, Card, Col, Input, Row } from "antd";
+import axios from "axios";
+import { useState } from "react";
+import { Layout } from "../Layout";
+
+const backendUrl = process.env.REACT_APP_AI_WORKFLOW_URL ?? "http://localhost:8000";
+
+const Dashboard = () => {
+  const [content, setContent] = useState("");
+  const [result, setResult] = useState();
+
+  const handleOnSendData = async () => {
+    try {
+      const res = await axios.post(`${backendUrl}`, JSON.parse(content), {
+        auth: {
+          username: process.env.REACT_APP_AI_USERNAME ?? "",
+          password: process.env.REACT_APP_AI_PASSWORD ?? "",
+        },
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      setResult(res.data);
+    } catch (error) {}
+  };
+
+  return (
+    <Layout>
+      <Row
+        justify="center"
+        align="middle"
+        style={{ width: "100%", height: "100%" }}
+      >
+        <Col xs={22} md={16} lg={10}>
+          <Card title="H1st AI App with React UI">
+            <Row gutter={[24, 24]}>
+              <Col span={24}>
+                <Input.TextArea onChange={(e) => setContent(e.target.value)} />
+              </Col>
+              <Col span={24}>
+                <Row justify="center">
+                  <Button onClick={handleOnSendData}>Send Data</Button>
+                </Row>
+              </Col>
+              <Col span={24}>Result:</Col>
+              <Col span={24}>{JSON.stringify(result)}</Col>
+            </Row>
+          </Card>
+        </Col>
+      </Row>
+    </Layout>
+  );
+};
+
+export default Dashboard;
