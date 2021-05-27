bf6d4169834a52cbe8891c31750263dcc1349288
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-05-26 18:17:41 -0700

    fix h1st-with-react-ui

--- h1st-with-react-ui/h1st_ai/urls.py
+++ h1st-with-react-ui/h1st_ai/urls.py
@@ -2,10 +2,13 @@ from django.urls.conf import include, path
 
 from rest_framework.routers import DefaultRouter
 
+from .views import FirstModelAPIView
+
 
 ROUTER = DefaultRouter()
 
 
 urlpatterns = [
-    path('', include(ROUTER.urls))
+    path('', include(ROUTER.urls)),
+    path('first-model/', FirstModelAPIView.as_view())
 ]
