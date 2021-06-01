606f57b0dbc380606899ebbd6681d2663387420b
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-05-31 21:33:14 -0700

    update h1st-with-model-io-mapping-ui/h1st_ai/scripts/train_my_model

--- h1st-with-model-io-mapping-ui/h1st_ai/scripts/train_my_model.py
+++ h1st-with-model-io-mapping-ui/h1st_ai/scripts/train_my_model.py
@@ -3,4 +3,8 @@ from h1st_ai.models import MyModel
 
 def run():
     model = MyModel()
-    print(model)
+
+    train_data, val_data, _ = model.load_data()
+    train_ds, val_ds = model.prep_data(data=(train_data, val_data))
+
+    model.train(data=(train_ds, val_ds))
